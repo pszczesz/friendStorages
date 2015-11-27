45ef11f36ad641b1f80b3a2168ffd75097eb1370
pszczesz <pszczesz@poczta.onet.pl>
2015-11-27 13:24:05 +0100

    alpha1.1

--- FriendStories.UI/DataProvider/Lookups/ILookupProvider.cs
+++ FriendStories.UI/DataProvider/Lookups/ILookupProvider.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 
 namespace FriendStories.UI.DataProvider.Lookups {
-    public interface ILookupProvider {
+    public interface ILookupProvider<T> {
         IEnumerable<LookupItem> GetLookup();
     }
 }
--- FriendStories.UI/DataProvider/Lookups/ILookupProvider1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-
-namespace FriendStories.UI.DataProvider.Lookups
-{
-    public interface ILookupProvider<T> {
-        IEnumerable<LookupItem> GetLookup();
-    }
-}
